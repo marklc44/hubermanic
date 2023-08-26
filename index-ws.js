f3903c59ee2903616fbf29f495e2421b9ade30f1
Mark <marklc44@gmail.com>
2023-08-26 11:46:21 -0700

    add db logging

--- index-ws.js
+++ index-ws.js
@@ -8,6 +8,16 @@ app.get('/', function(req, res) {
 
 server.on('request', app)
 server.listen(3000, function() { console.log('Server started on port 3000') });
+process.on('SIGINT', () => {
+    wss.clients.forEach(function each(client) {
+        client.close();
+    });
+    console.log('before server close')
+    server.close(() => {
+        console.log('before shutdown db')
+        shutdownDB();
+    });
+});
 
 /** Begin websockets */
 const WebSocketServer = require('ws').Server;
@@ -31,6 +41,10 @@ wss.on('connection', function connection(ws) {
         ws.send('Welcome to my server');
     }
 
+    db.run(`INSERT INTO visitors (count, time)
+        VALUES (${numClients}, datetime('now'))
+    `);
+
     ws.on('close', function close() {
         wss.broadcast(`A client has disconnected`);
         console.log('A client has disconnected');
@@ -41,4 +55,35 @@ wss.broadcast = function broadcast(data) {
     wss.clients.forEach(function each(client) {
         client.send(data);
     });
+}
+
+/** End websockets */
+/** Begin database */
+// sqlite doesn't require a separate db server
+// just an npm package
+// in memory means the db will be destroyed when the server is restarted
+const sqlite = require('sqlite3');
+const db = new sqlite.Database(':memory:');
+
+db.serialize(() => {
+    db.run(`
+        CREATE TABLE visitors (
+            count INTEGER,
+            time TEXT
+        )
+    `);
+});
+
+// never run sql directly, only with prepared statements or functions like this
+function getCounts() {
+    db.each('SELECT * FROM visitors', (err, row) => {
+        console.log(row)
+    });
+}
+
+function shutdownDB() {
+    console.log('before get counts')
+    getCounts();
+    console.log('Shutting down db');
+    db.close();
 }
